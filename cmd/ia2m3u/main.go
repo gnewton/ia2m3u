5400a92513bb9e437b2db02f924f6395b7ba732c
Glen Newton <glen.newton@gmail.com>
2026-03-12 07:06:20 -0400

    Removed context

--- cmd/ia2m3u/main.go
+++ cmd/ia2m3u/main.go
@@ -94,7 +94,7 @@ func main() {
 
 				ctxItem, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 				//_, _, err = ia.GetItem(ctxItem, results[i].Identifier, client, itemCache)
-				_, err = ia.GetItem(ctxItem, results[i].Identifier, client, nil)
+				_, err = ia.GetItem(ctxItem, results[i].Identifier, client, itemCache)
 				if err != nil {
 					log.Fatal(err)
 				} else {
